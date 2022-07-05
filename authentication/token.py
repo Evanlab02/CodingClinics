fb2a3312a2ff923f84de6de752894adfd0687778
Evan Labuschagne <Evanlab02@gmail.com>
2022-07-05 11:10:20 +0200

    Updated Login
    The login will now first check if the credentials are valid and expired. If the credentials are valid and not expired, it will confirm if the user wants to login again

--- authentication/token.py
+++ authentication/token.py
@@ -1,11 +1,10 @@
 """Module containing all functions related to writing and reading
 the token.json"""
 
-import sys
-
 from datetime import datetime
 from rich import print as rprint
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
 from first_time_setup.fts import path_exists
 
 def create_token(storage_directory: str, permission):
@@ -16,20 +15,10 @@ def create_token(storage_directory: str, permission):
     goes through the oauth process, it will create a token.json
     meaning the user is signed in"""
 
-    do_login = True
-
-    if check_token(storage_directory):
-        do_login = input("You are already logged in, do you wish to log in in again [y/n]: ")\
-            .lower() == "y"
-
-    if not do_login:
-        sys.exit("Cancelled Login!")
-
     flow = InstalledAppFlow.from_client_secrets_file(
         f'{storage_directory}credentials.json', permission)
 
     creds = flow.run_local_server(port=0)
-
     rprint("[cyan]Writing Token[/cyan]")
 
     with open(f'{storage_directory}token.json', 'w', encoding="utf-8") as token:
@@ -40,5 +29,19 @@ def create_token(storage_directory: str, permission):
         log_file.writelines(f"-->    [{datetime.now()}]: Permissions: {permission}\n")
 
 
-def check_token(storage_directory: str):
-    return path_exists(f"{storage_directory}token.json")
+def token_valid(creds):
+    return creds and creds.valid
+
+
+def token_expired(creds):
+    return creds and creds.expired and creds.refresh_token
+
+
+def check_token(creds):
+    return token_valid(creds) and not token_expired(creds)
+        
+
+def load_token(storage_directory: str, permission):
+    if path_exists(f'{storage_directory}token.json'):
+        creds = Credentials.from_authorized_user_file(f'{storage_directory}token.json', permission)
+        return creds
