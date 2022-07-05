2a5aab5347c8ec21e0f57ea9ee5a5865e5383960
Evan Labuschagne <Evanlab02@gmail.com>
2022-07-05 10:44:19 +0200

    Update token.py
    When logging in, if a token.json exists, it considers the user logged in. It will then promt and confirm if the user wants to log in again. - More checks to be added

--- authentication/token.py
+++ authentication/token.py
@@ -1,9 +1,12 @@
 """Module containing all functions related to writing and reading
 the token.json"""
 
+import sys
+
 from datetime import datetime
 from rich import print as rprint
 from google_auth_oauthlib.flow import InstalledAppFlow
+from first_time_setup.fts import path_exists
 
 def create_token(storage_directory: str, permission):
     """Creates a token (token.json) using oauth to login
@@ -12,6 +15,15 @@ def create_token(storage_directory: str, permission):
     to determine what permissions the user should have, when the user
     goes through the oauth process, it will create a token.json
     meaning the user is signed in"""
+
+    do_login = True
+
+    if check_token(storage_directory):
+        do_login = input("You are already logged in, do you wish to log in in again [y/n]: ")\
+            .lower() == "y"
+
+    if not do_login:
+        sys.exit("Cancelled Login!")
 
     flow = InstalledAppFlow.from_client_secrets_file(
         f'{storage_directory}credentials.json', permission)
@@ -26,3 +38,7 @@ def create_token(storage_directory: str, permission):
     with open(f"{storage_directory}log.txt", "a", encoding="utf-8") as log_file:
         log_file.writelines(f"[{datetime.now()}]: Created new token\n")
         log_file.writelines(f"-->    [{datetime.now()}]: Permissions: {permission}\n")
+
+
+def check_token(storage_directory: str):
+    return path_exists(f"{storage_directory}token.json")
