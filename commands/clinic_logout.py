4b2a44819962d1d2300d864d0c07423f5eedd2a8
Evan Labuschagne <Evanlab02@gmail.com>
2022-07-06 12:55:20 +0200

    Integrated Removal Of Token

--- commands/clinic_logout.py
+++ commands/clinic_logout.py
@@ -5,6 +5,7 @@ import sys
 
 from datetime import datetime
 from rich import print as rprint
+from authentication.token import remove_token
 from first_time_setup.fts import get_home_directory, get_storage_directory
 
 def start_logout(api_details: dict, storage_directory: str):
@@ -35,5 +36,6 @@ def do_logout(api_details: dict):
     """Does all Logout related functions - main function for this module"""
     storage_directory = get_storage_directory(get_home_directory())
     start_logout(api_details, storage_directory)
+    remove_token(storage_directory)
     end_logout(api_details, storage_directory)
     sys.exit()
