e6149a70866985acb3f73341cce6cd1c112223ea
Evan Labuschagne <Evanlab02@gmail.com>
2022-07-06 12:07:16 +0200

    Added Logout End Log

--- commands/clinic_logout.py
+++ commands/clinic_logout.py
@@ -1,6 +1,7 @@
 """
 Houses all the logic for the logout command
 """
+import sys
 
 from datetime import datetime
 from rich import print as rprint
@@ -19,7 +20,20 @@ def start_logout(api_details: dict, storage_directory: str):
     rprint("[magenta]Logging out...[/magenta]")
 
 
+def end_logout(api_details: dict, storage_directory: str):
+    """Logs that that the user is logged out"""
+    permission = api_details["permissions"]
+
+    with open(f"{storage_directory}log.txt", "a", encoding="utf-8") as log_file:
+        log_file.writelines(f"[{datetime.now()}]: Logged Out\n")
+        log_file.writelines(f"-->    [{datetime.now()}]: Permissions: {permission}\n")
+
+    rprint("[green]Logged Out[/green]")
+
+
 def do_logout(api_details: dict):
     """Does all Logout related functions - main function for this module"""
     storage_directory = get_storage_directory(get_home_directory())
     start_logout(api_details, storage_directory)
+    end_logout(api_details, storage_directory)
+    sys.exit()
