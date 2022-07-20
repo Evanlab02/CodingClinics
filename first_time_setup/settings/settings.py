b8dc48d86e979ec68044ec887849392774e1ace7
Evan Labuschagne <Evanlab02@gmail.com>
2022-07-20 21:03:41 +0200

    Added More Comprehensive Checks For Settings File

--- first_time_setup/settings/settings.py
+++ first_time_setup/settings/settings.py
@@ -4,6 +4,35 @@ File responsible for creating and editing settings file based on preferences
 
 from file_helpers.json_helper import read_json
 
+def check_settings(settings: dict):
+    """Checks that the settings file is complete"""
+    settings_keys = settings.keys()
+
+    if not check_keys(settings_keys):
+        return False
+    
+    all_checks = [
+        check_data_format(settings)
+        ]
+
+    return not False in all_checks
+
+
+def check_keys(key_values: set):
+    """Checks that the required keys are in the settings file"""
+    calendar_id_key = "calendarID" in key_values
+    data_format_key = "DATA FORMAT" in key_values
+    all_checks = [calendar_id_key, data_format_key]
+    return not False in all_checks
+
+
+def check_data_format(settings: dict):
+    """Checks that the data format value is correct"""
+    supported_formats = ["JSON"]
+    data_format = settings["DATA FORMAT"]
+    return data_format in supported_formats
+
+
 def load_settings(storage_directory: str, file_name: str):
     """Loads specified file at specified directory"""
     file_path = storage_directory + file_name
--- testing/test_settings.py
+++ testing/test_settings.py
@@ -20,3 +20,53 @@ class MyTestCase(unittest.TestCase):
         expected = {"TEST LOAD": "LOAD TEST"}
         actual = settings.load_settings(self.STORAGE_DIRECTORY, file_name)
         self.assertEqual(expected, actual)
+
+
+    @patch("sys.stdout", StringIO())
+    def test_check_valid_settings_combination_1(self):
+        """Tests check_settings function on a valid settings file"""
+        mock_settings = {"calendarID": "ABCD", "DATA FORMAT": "JSON"}
+        actual = settings.check_settings(mock_settings)
+        self.assertEqual(True, actual)
+
+
+    @patch("sys.stdout", StringIO())
+    def test_check_invalid_settings_no_data_formatting(self):
+        """Tests check_settings function on a invalid settings file with no 
+        data formatting key-value pair"""
+        mock_settings = {"calendarID": "ABCD"}
+        actual = settings.check_settings(mock_settings)
+        self.assertEqual(False, actual)
+
+
+    @patch("sys.stdout", StringIO())
+    def test_check_valid_settings_keys(self):
+        """Tests check_keys function on a valid settings file"""
+        mock_settings = {"calendarID": "", "DATA FORMAT": ""}
+        actual = settings.check_keys(mock_settings.keys())
+        self.assertEqual(True, actual)
+
+
+    @patch("sys.stdout", StringIO())
+    def test_check_invalid_settings_keys(self):
+        """Tests check_keys function on a invalid settings file with no 
+        data format key-value pair"""
+        mock_settings = {"calendarID": ""}
+        actual = settings.check_keys(mock_settings.keys())
+        self.assertEqual(False, actual)
+
+
+    @patch("sys.stdout", StringIO())
+    def test_check_json_data_format(self):
+        """Tests check_data_format function on a valid data format - JSON"""
+        mock_settings = {"DATA FORMAT": "JSON"}
+        actual = settings.check_data_format(mock_settings)
+        self.assertEqual(True, actual)
+
+
+    @patch("sys.stdout", StringIO())
+    def test_check_invalid_data_format(self):
+        """Tests check_data_format function on a invalid data format - xml"""
+        mock_settings = {"DATA FORMAT": "XML"}
+        actual = settings.check_data_format(mock_settings)
+        self.assertEqual(False, actual)
